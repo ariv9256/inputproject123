82dcbac689cacf30f24a64e74df89a6165221792
Angela Rivas <snowcats001@gmail.com>
2018-11-06 12:23:52 -0700

    added validint method in controller and a catch number value method. fixed gui message components.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -7,92 +7,91 @@ public class Controller
 	public void start()
 	{
 		System.out.println("Words get printed below");
-		
-		questions();
-		
-		moreInput();
-		
-		public boolean validInt(String sample)
-		{
-			boolean isValid = false;
-			
-			try
-			{
-				Integer.parseInt(sample);
-				isValid = true;
-			}
 
-		catch(NumberFormatException error)
+		questions();
+
+		moreInput();
+	}
+
+	public boolean validInt(String sample)
+	{
+		boolean isValid = false;
+
+		try
+		{
+			Integer.parseInt(sample);
+			isValid = true;
+		}
+
+		catch (NumberFormatException error)
 		{
 			System.out.println("You need to type in a number");
 		}
-		
+
 		return isValid;
 	}
-	
-		public boolean validDouble(String example)
+
+	public boolean validDouble(String example)
+	{
+		boolean isValid = false;
+
+		try
 		{
-			boolean isValid = false; 
-			
-			try
-			{
-				Double.parseDouble(example);
-				isValid = true;
-			}
-			catch(NumberFormatException oops)
-			{
-				System.out.println("Only floating point values are accepted/ numbers");
-			}
-			
-			return true;
-			
+			Double.parseDouble(example);
+			isValid = true;
 		}
-		
-		
+		catch (NumberFormatException oops)
+		{
+			System.out.println("Only floating point values are accepted/ numbers");
+		}
+
+		return isValid;
+
+	}
+
 	private void questions()
 	{
 		System.out.println("What is your favorite color?");
 		Scanner inputScanner = new Scanner(System.in);
-		
+
 		String answer = inputScanner.nextLine();
-		System.out.println(answer); 
-		
+		System.out.println(answer);
+
 		System.out.println("How are you today?");
 		answer = inputScanner.next();
-		
+
 		System.out.println(answer);
-		
+
 		System.out.println("What are you doing right now?");
 		answer = inputScanner.next();
-		
+
 		System.out.println(answer);
-		
-		
+
 		System.out.println("Do you like pizza??");
 		answer = inputScanner.next();
-		
+
 		System.out.println(answer);
-		
-//		inputScanner.close();
-		
+
+		// inputScanner.close();
+
 	}
-		private void moreInput()
+
+	private void moreInput()
 	{
-		Scanner textScanner; //This is a declaration of a scanner variable.
-		
-		textScanner = new Scanner(System.in); //Initialize of the textScanner variable
-		
+		Scanner textScanner; // This is a declaration of a scanner variable.
+
+		textScanner = new Scanner(System.in); // Initialize of the textScanner variable
+
 		System.out.println("Type in your favorite number");
-		
+
 		int userNumber = textScanner.nextInt();
-		
+
 		System.out.println("Your favorite nubmer is: " + userNumber);
-		
+
 		System.out.println("Type in some more information");
-		
+
 		textScanner.nextLine();
-		
+
 		textScanner.close();
 	}
 }
-
